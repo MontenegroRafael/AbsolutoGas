f6cd4d532a1bb3f73c47d2182fba5ddb4da1e12c
Rafael Montenegro <rafaelmrc3@gmail.com>
2022-09-26 19:13:23 -0300

    Rev.08 Client Models - OK

---
+++ Client/Models/Motorista.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.Models
+{
+    public class Motorista
+    {
+        public int IdMotorista { get; set; }
+        public string Nome { get; set; }
+        public string CNH { get; set; }
+        public string Telefone { get; set; }
+    }
+}
---
+++ Client/Models/Veiculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.Models
+{
+    public class Veiculo
+    {
+        public int IdVeiculo { get; set; }
+        public string Placa { get; set; }
+        public int IdMotorista { get; set; }
+
+    }
+}
